59b9267cb0affb8cac1352f42e9629118c8298e5
Dherkyan Ribeiro <dherkyan@gmail.com>
2019-08-22 16:47:40 -0400

    Add classe AppContexto

---
+++ TDSTecnologia.Site.Infrastructure/Data/AppContexto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TDSTecnologia.Site.Core.Entities;
+
+namespace TDSTecnologia.Site.Infrastructure.Data
+{
+    public class AppContexto : DbContext
+    {
+        public AppContexto(DbContextOptions<AppContexto> opcoes) : base(opcoes)
+        {
+        }
+
+        public DbSet<Curso> CursoDao { get; set; }
+    }
+}
