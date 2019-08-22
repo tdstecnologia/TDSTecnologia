af7e5650ce39121a8d8681e1b485cb7560371c23
Dherkyan Ribeiro <dherkyan@gmail.com>
2019-08-22 16:50:45 -0400

    Add anotações na classe Curso

--- TDSTecnologia.Site.Core/Entities/Curso.cs
+++ TDSTecnologia.Site.Core/Entities/Curso.cs
@@ -1,19 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TDSTecnologia.Site.Core.Entities
 {
+    [Table("tb01_curso")]
     public class Curso
     {
+        [Key]
+        [Column("id")]
         public int Id { get; set; }
 
+        [Column("nome")]
         public string Nome { get; set; }
 
+        [Column("descricao")]
         public string Descricao { get; set; }
 
+        [Column("quantidade_aula")]
         public int QuantidadeAula { get; set; }
 
+        [Column("data_inicio")]
         public DateTime DataInicio { get; set; }
     }
 }
