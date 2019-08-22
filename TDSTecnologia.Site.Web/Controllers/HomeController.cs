fe9fe662774454dab013890cc6309ab7344152d2
Dherkyan Ribeiro <dherkyan@gmail.com>
2019-08-22 17:36:42 -0400

    Add listagem de cursos

--- TDSTecnologia.Site.Web/Controllers/HomeController.cs
+++ TDSTecnologia.Site.Web/Controllers/HomeController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using TDSTecnologia.Site.Infrastructure.Data;
 
 namespace TDSTecnologia.Site.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly AppContexto _context;
+
+        public HomeController(AppContexto context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.CursoDao.ToListAsync());
         }
     }
 }
