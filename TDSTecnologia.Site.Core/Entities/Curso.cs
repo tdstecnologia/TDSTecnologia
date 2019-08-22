6678583cf1e054a23fdb28ff344c911a820a33d4
Dherkyan Ribeiro <dherkyan@gmail.com>
2019-08-22 15:51:02 -0400

    Add classe Curso

---
+++ TDSTecnologia.Site.Core/Entities/Curso.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDSTecnologia.Site.Core.Entities
+{
+    public class Curso
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public int QuantidadeAula { get; set; }
+
+        public DateTime DataInicio { get; set; }
+    }
+}
