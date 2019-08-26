af20bb8243cf06475d1c2528c7713bb691a68d6c
Dherkyan Ribeiro <dherkyan@gmail.com>
2019-08-26 15:25:50 -0400

    Add anotação descrição do enum

---
+++ TDSTecnologia.Site.Core/Dominio/Extensoes/EnumDescricao.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TDSTecnologia.Site.Core.Dominio.Extensoes
+{
+    public class EnumDescricao : Attribute
+    {
+        public string Value { get; private set; }
+
+        public EnumDescricao(string value)
+        {
+            Value = value;
+        }
+    }
+}
