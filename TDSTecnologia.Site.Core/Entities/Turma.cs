1731bfb6c861415634841459bed92e4592eb5337
Dherkyan Ribeiro <dherkyan@gmail.com>
2019-08-29 14:33:42 -0400

    Add classe Turma

---
+++ TDSTecnologia.Site.Core/Entities/Turma.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDSTecnologia.Site.Core.Entities
+{
+    public class Turma
+    {
+        public int Id { get; set; }
+
+        public int Descricao { get; set; }
+
+        public DateTime DataInicio { get; set; }
+    }
+}
