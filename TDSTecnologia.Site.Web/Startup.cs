ae3a7797c78c21dbff8a1c45e078cb22e6269ee3
dherkyan <dherkyan@gmail.com>
2019-09-08 23:02:43 -0400

    Configurar acesso classe Startup

--- TDSTecnologia.Site.Web/Startup.cs
+++ TDSTecnologia.Site.Web/Startup.cs
@@ -1,10 +1,14 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.SpaServices;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 using TDSTecnologia.Site.Core.Entities;
 using TDSTecnologia.Site.Infrastructure.Data;
 using TDSTecnologia.Site.Infrastructure.Services;
@@ -19,12 +23,9 @@ namespace TDSTecnologia.Site.Web
         {
             Configuration = configuration;
         }
+
         public void ConfigureDevelopmentServices(IServiceCollection services)
         {
-            services.AddScoped<CursoService, CursoService>();
-            services.AddScoped<UsuarioService, UsuarioService>();
-            services.AddScoped<PermissaoService, PermissaoService>();
-
             services.AddMvc();
             services.AddEntityFrameworkNpgsql()
          .AddDbContext<AppContexto>(options => options.UseNpgsql(Configuration.GetConnectionString("AppConnection")));
@@ -32,17 +33,49 @@ namespace TDSTecnologia.Site.Web
             services.AddIdentity<Usuario, Permissao>()
                             .AddDefaultUI(UIFramework.Bootstrap4)
                             .AddEntityFrameworkStores<AppContexto>();
+
+            services.AddScoped<CursoService, CursoService>();
+            services.AddScoped<UsuarioService, UsuarioService>();
+            services.AddScoped<PermissaoService, PermissaoService>();
+
+            
+
+            services.ConfigureApplicationCookie(opcoes =>
+            {
+                opcoes.Cookie.HttpOnly = true;
+                opcoes.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+                opcoes.LoginPath = "/Usuarios/Login";
+                opcoes.LogoutPath = "/Usuarios/logout";
+                opcoes.SlidingExpiration = true;
+            });
+
+            
         }
 
         public void ConfigureProductionServices(IServiceCollection services)
         {
             services.AddMvc();
             services.AddEntityFrameworkNpgsql()
-         .AddDbContext<AppContexto>(options => options.UseNpgsql(Databases.Instance.Conexao));
+         .AddDbContext<AppContexto>(options => options.UseNpgsql(Configuration.GetConnectionString("AppConnection")));
+
+            services.AddIdentity<Usuario, Permissao>()
+                            .AddDefaultUI(UIFramework.Bootstrap4)
+                            .AddEntityFrameworkStores<AppContexto>();
 
             services.AddScoped<CursoService, CursoService>();
             services.AddScoped<UsuarioService, UsuarioService>();
             services.AddScoped<PermissaoService, PermissaoService>();
+
+
+
+            services.ConfigureApplicationCookie(opcoes =>
+            {
+                opcoes.Cookie.HttpOnly = true;
+                opcoes.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+                opcoes.LoginPath = "/Usuarios/Login";
+                opcoes.LogoutPath = "/Usuarios/logout";
+                opcoes.SlidingExpiration = true;
+            });
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
