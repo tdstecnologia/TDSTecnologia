b88bf4a2052a1a5a51bf45e3c73c5473c0717b0a
Dherkyan Ribeiro <dherkyan@gmail.com>
2019-09-12 18:55:55 -0400

    Add serviços de manipulçao de Erros

---
+++ TDSTecnologia.Site.Web/Controllers/Erros.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TDSTecnologia.Site.Web.Controllers
+{
+    public class Erros : AppAbstractController
+    {
+        [HttpGet("Erros/{codigo}")]
+        public IActionResult Index(int codigoErro)
+        {
+            return View(codigoErro);
+        }
+    }
+}
